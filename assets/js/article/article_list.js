5917d4409f57d201151986c07fd3d58dc5561e8e
dachaoyue <2569062071@qq.com>
2021-07-23 11:45:59 +0800

    第一阶段结束

--- assets/js/article/article_list.js
+++ assets/js/article/article_list.js
@@ -6,4 +6,24 @@ $(function() {
         cate_id: '',
         state: ''
     }
+
+    initList();
+    //获取文章列表
+    function initList() {
+        $.ajax({
+            method: 'GET',
+            url: '/my/article/list',
+            data: q,
+            success: function(res) {
+                if(res.status !== 0) {
+                    layui.layer.msg('获取文章列表失败')
+                }
+               
+                layui.layer.msg('获取文章列表成功')
+
+                var htmlStr = template('tpl-artList',res);
+                $('tbody').html(htmlStr);
+            }
+        })
+    }
 })
